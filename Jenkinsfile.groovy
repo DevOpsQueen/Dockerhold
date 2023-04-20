af55166766d65c5b1e6b62dedb4b2e9727a7f3de
DevOpsQueen <110736912+DevOpsQueen@users.noreply.github.com>
2023-04-20 10:29:10 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,28 +12,26 @@ pipeline {
         stage('Build Docker Image') {
             steps {
                 script {
-                    withCredentials([usernamePassword(credentialsId: 'f347aa87-41e5-40ab-b0e4-51ca2b46a34b', passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
-                        def dockerfile = """
-                            FROM tomcat:9-jre8
-                            EXPOSE ${DOCKER_PORT}
-                            ENV TOMCAT_USER=admin
-                            ENV TOMCAT_PASSWORD=password
-                            ENV WAR_FILE=ABCTech.war
-                            COPY ABCTech.war /usr/local/tomcat/webapps/
-                            ADD deploy.sh /usr/local/tomcat/bin/
-                            RUN chmod +x /usr/local/tomcat/bin/deploy.sh
-                            CMD ["/usr/local/tomcat/bin/deploy.sh"]
-                        """
-                        sh "sudo docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} -"
-                    }
+                    def dockerfile = """
+                        FROM tomcat:9-jre8
+                        EXPOSE ${DOCKER_PORT}
+                        ENV TOMCAT_USER=admin
+                        ENV TOMCAT_PASSWORD=password
+                        ENV WAR_FILE=ABCTech.war
+                        COPY ABCTech.war /usr/local/tomcat/webapps/
+                        ADD deploy.sh /usr/local/tomcat/bin/
+                        RUN chmod +x /usr/local/tomcat/bin/deploy.sh
+                        CMD ["/usr/local/tomcat/bin/deploy.sh"]
+                    """
+                    sh 'sudo docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} -'
                 }
             }
         }
         
         stage('Push Docker Image') {
             steps {
-                script {
-                    withCredentials([usernamePassword(credentialsId: 'DOCKERHUB_CREDENTIAL_ID', passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
+                withCredentials([usernamePassword(credentialsId: 'f347aa87-41e5-40ab-b0e4-51ca2b46a34b', passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
+                    script {
                         sh "docker login -u ${DOCKER_REGISTRY} -p ${DOCKERHUB_PASSWORD}"
                         sh "docker push ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
                     }
@@ -44,12 +42,9 @@ pipeline {
         stage('Run Docker Container') {
             steps {
                 script {
-                    withCredentials([usernamePassword(credentialsId: 'DOCKERHUB_CREDENTIAL_ID', passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
-                        sh "docker run -d -p ${DOCKER_PORT}:${DOCKER_PORT} -e TOMCAT_USER=admin -e TOMCAT_PASSWORD=password -e WAR_FILE=ABCTech.war ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
-                    }
+                    sh "docker run -d -p ${DOCKER_PORT}:${DOCKER_PORT} -e TOMCAT_USER=admin -e TOMCAT_PASSWORD=password -e WAR_FILE=ABCTech.war ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
                 }
             }
         }
     }
 }
-
